b755269060cbc77b65294aee452bd345367af6db
Sajid Khan Afridi <uetianafridi@gmail.com>
2023-08-26 15:00:24 +0500

    ain

---
+++ extra_work/git.sql
@@ -0,0 +1,8 @@
+1. create repo at github and copy the link
+2. create next app in a folder
+3. execute the following commands in terminal
+    $   pwd
+    $   git remote -v
+    $   git remote add origin git@github.com/sajid-khan-afridi/hackathon_zero.git
+    $   git add --all
+    $   git push origin main --force
